ce130f365f1b08f5fcae93f11a68342fbd5795e6
Grendel <jcg@grendel.world>
2022-04-07 16:26:53 -0500

    Add Kuniverse experiment

---
+++ Kuniverse/Orbiter.gd
@@ -0,0 +1,17 @@
+extends Spatial
+#fucki
+
+
+export(float) var ECCENTRICITY := 1.0
+export(float) var ORBIT_SPEED  := 1.0  # orbits per minute
+
+onready var orbit_spinner := $OrbitSpinner as Spatial
+
+
+func _ready() -> void:
+	scale.x = ECCENTRICITY
+	ORBIT_SPEED /= 60.0
+
+
+func _process(delta:float) -> void:
+	orbit_spinner.rotate_y(2*PI*ORBIT_SPEED * delta)
